dee4260596e0276deb7ba5e03a1d29534eec39e6
Ed Shadi <edshadi@gmail.com>
2014-09-10 12:58:20 -0700

    basic index template with jade

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'jade');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.render('index', {youAreUsingJade: true});
 })
 
 app.listen(app.get('port'), function() {
